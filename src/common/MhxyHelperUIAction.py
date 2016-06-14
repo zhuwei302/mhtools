31370f53a918490d4c8cabbc66040cbdd8188d17
zhuwei <244840434@qq.com>
2016-06-14 17:03:11 +0800

    修改弹窗交互

--- src/common/MhxyHelperUIAction.py
+++ src/common/MhxyHelperUIAction.py
@@ -2,11 +2,15 @@ __author__ = 'zhuwei'
 #coding=utf-8
 
 import MhxyHelperUIService
+import tkMessageBox
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 def beginYb(kj):
     id = kj.get()
-    print(id)
-    if id == "请输入游戏ID":
-        print("请输入id")
+    # print(id)
+    if "请输入游戏ID" in id:
+        tkMessageBox.showerror(title="错误", message="请输入数字ID")
     else:
         MhxyHelperUIService.getMHXYWinById(id)
