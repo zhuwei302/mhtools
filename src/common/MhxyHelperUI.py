bc25d59c55d8b1325c090866a4c886be1c39f9d8
zhuwei <244840434@qq.com>
2016-06-14 16:17:50 +0800

    修改布局代码

--- src/common/MhxyHelperUI.py
+++ src/common/MhxyHelperUI.py
@@ -13,10 +13,7 @@ root = Tk(className="MhxyHelper") #窗口标题
 
 
 '''标签'''
-label = Label(root) #创建标签
-label['text'] = "游戏ID:" #设置标签的text属性值为 "游戏ID"
-label.grid(row=0,column=0)
-# label.pack() #显示标签
+label = Label(root,text="游戏ID").grid(row=0,column=0) #创建标签
 
 
 '''文本框'''
@@ -25,7 +22,7 @@ text.set("请输入游戏ID")
 entry = Entry(root)
 entry['textvariable'] = text
 entry.grid(row=0,column=1)
-# entry.pack()
+
 
 '''按钮'''
 button = Button(root,text="开始押镖",command=lambda:MhxyHelperUIAction.beginYb(entry)).grid(row=1,column=1)
